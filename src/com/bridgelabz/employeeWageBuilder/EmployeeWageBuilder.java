2080419a5384a06da47f06832564c38d848fe16a
Textspot <sachinkatarki1995@gmail.com>
2021-11-27 14:56:27 +0530

    Added:Calculate the Employee Wages for a Month in programme

--- src/com/bridgelabz/employeeWageBuilder/EmployeeWageBuilder.java
+++ src/com/bridgelabz/employeeWageBuilder/EmployeeWageBuilder.java
@@ -5,6 +5,7 @@ public class EmployeeWageBuilder {
 	public static final int Is_Part_Time = 1;
 	public static final int Is_Full_Time = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int Num_Of_Working_Days = 20;
 
 	public static void main(String[] args) {
 		EmployeeWageBuilder employeewage = new EmployeeWageBuilder();
@@ -15,23 +16,27 @@ public class EmployeeWageBuilder {
 	public void welcome() {
 	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 	}
-
-	public void emopDailyWage() {
+	public void emopDailyWage(){
 		int empHrs = 0;
 		int empWage = 0;
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-		case Is_Part_Time:
-			empHrs = 4;
-			break;
-		case Is_Full_Time:
-			empHrs = 8;
-			break;
-		default:
-			empHrs = 0;
+		int totalEmpWage = 0;
+		for (int day = 1; day <= Num_Of_Working_Days; day++) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case Is_Part_Time:
+				empHrs = 4;
+				break;
+			case Is_Full_Time:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Employee_wage:" + empWage);
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Employee_wage:" + empWage);
-
+		System.out.println("Total Employee_wage:" + totalEmpWage);
 	}
+	
 }
