6cda31c6498a460b6fbc2087ee9c9316632fba51
Textspot <sachinkatarki1995@gmail.com>
2021-11-27 12:20:12 +0530

    Added:Random Check the Employe is Present Or Absent

--- src/com/bridgelabz/employeeWageBuilder/EmployeeWageBuilder.java
+++ src/com/bridgelabz/employeeWageBuilder/EmployeeWageBuilder.java
@@ -1,5 +1,22 @@
 package com.bridgelabz.employeeWageBuilder;
 
 public class EmployeeWageBuilder {
+	public static void main(String[] args) {
+		welcome();
+		attendanceCheck();
+	}
+
+	static void welcome() {
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	}
+
+	static void attendanceCheck() {
+		int isFullTime = 1;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isFullTime)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+	}
 
 }
