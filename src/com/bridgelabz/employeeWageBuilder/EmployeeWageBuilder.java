5f53e723e0ba09151b483fcd792a283e2434fe86
Textspot <sachinkatarki1995@gmail.com>
2021-11-27 14:37:17 +0530

    Added:Using Collections Add to Calculate the Part Time Employee Wages

--- src/com/bridgelabz/employeeWageBuilder/EmployeeWageBuilder.java
+++ src/com/bridgelabz/employeeWageBuilder/EmployeeWageBuilder.java
@@ -1,27 +1,31 @@
 package com.bridgelabz.employeeWageBuilder;
 
 public class EmployeeWageBuilder {
-	public static void main(String[] args) {
-		EmployeeWageBuilder employeewage=new EmployeeWageBuilder();
-		employeewage.welcome();
-		employeewage.emopDailyWage();
-	}
 
-	public void welcome() {
-		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-	}
+		public static void main(String[] args) {
+			EmployeeWageBuilder employeewage = new EmployeeWageBuilder();
+			employeewage.welcome();
+			employeewage.emopDailyWage();
+		}
 
-	public void emopDailyWage() {
-		int Is_Full_Time = 1;
-		int EMP_RATE_PER_HOUR = 20;
-		int empHrs = 0;
-		int empWage = 0;
-		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-		if (empCheck == Is_Full_Time)
-			empHrs = 8;
-		else
-			empHrs = 0;
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Employee_wage:" + empWage);
-	}
+		public void welcome() {
+			System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		}
+
+		public void emopDailyWage() {
+			int Is_Part_Time = 1;
+			int Is_Full_Time = 2;
+			int EMP_RATE_PER_HOUR = 20;
+			int empHrs = 0;
+			int empWage = 0;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			if (empCheck == Is_Part_Time)
+				empHrs = 4;
+			else if (empCheck == Is_Full_Time)
+				empHrs = 8;
+			else
+				empHrs = 0;
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Employee_wage:" + empWage);
+		}
 }
